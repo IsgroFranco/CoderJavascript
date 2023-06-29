06d684f75a5909b97eb0607f0774e254c8d18014
IsgroFranco <isgro.franco1@gmail.com>
2023-06-29 20:58:08 -0300

    add cart funcionality

--- app.js
+++ app.js
@@ -1,66 +1,11 @@
-const productos = [
-  {
-    id: "air-jordan-1-mid-taxi",
-    titulo: "Air Jordan 1 Mid Taxi (GS) (Nordelta)",
-    imagen: "./assets/Jordan-1-Mid-Taxi-GS-433x433.jpg",
-    categoria: {
-      nombre: "nike",
-      id: "nike",
-    },
-    precio: 600,
-  },
-  {
-    id: "adidas-yeezy-1-boost-350-v2",
-    titulo: "Adidas Yeezy Boost 350 V2",
-    imagen: "./assets/adidas-Yeezy-Boost-350-V2-Beluga-Reflective.jpg",
-    categoria: {
-      nombre: "adidas",
-      id: "adidas",
-    },
-    precio: 680,
-  },
-  {
-    id: "adidas-forum-buckle-low",
-    titulo: "Adidas Forum Buckle Low",
-    imagen: "./assets/adidas-Forum-Buckle-Low-Bad-Bunny-Blue-Tint.jpg",
-    categoria: {
-      nombre: "adidas",
-      id: "adidas",
-    },
-    precio: 750,
-  },
-  {
-    id: "adidas-adi2000-yu-gi-oh",
-    titulo: "Adidas ADI2000 Yu-Gi-Oh!",
-    imagen:
-      "./assets/adidas-ADI2000-Yu-Gi-Oh-Yugi_s-World-with-Sealed-Dark-Magician-Promo-Card-433x433.jpg",
-    categoria: {
-      nombre: "adidas",
-      id: "adidas",
-    },
-    precio: 650,
-  },
-  {
-    id: "adidas-yeezy-knit-rnr-sulfur",
-    titulo: "Adidas Yeezy Knit RNR Sulfur",
-    imagen: "./assets/adidas-Yeezy-Knit-RNR-Sulfur-433x433.jpg",
-    categoria: {
-      nombre: "adidas",
-      id: "adidas",
-    },
-    precio: 500,
-  },
-  {
-    id: "nike-dunk-low-se-world",
-    titulo: "Nike Dunk Low SE World",
-    imagen: "./assets/Nike-Dunk-Low-SE-World-Champs-Black-White-433x433.jpg",
-    categoria: {
-      nombre: "nike",
-      id: "nike",
-    },
-    precio: 700,
-  },
-];
+let productos = [];
+
+fetch("./productos.json")
+  .then((resp) => resp.json())
+  .then((data) => {
+    productos = data;
+    cargarProductos(productos);
+  });
 
 const contenedorProductos = document.querySelector("#wrapper-products");
 const botonCategoria = document.querySelectorAll(".boton-categoria");
@@ -141,6 +86,19 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function agragarAlCarrito(e) {
+  Toastify({
+    text: "Producto agregado!",
+    duration: 2000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+
   const idBoton = e.currentTarget.id;
   const productoAgregado = productos.find(
     (producto) => producto.id === idBoton
@@ -158,7 +116,6 @@ function agragarAlCarrito(e) {
   localStorage.setItem("carrito", JSON.stringify(productosEnCarrito));
   actualizarCarrito();
   actualizarNumerito();
-  localStorage.setItem("carrito", JSON.stringify(productosEnCarrito));
 }
 
 function actualizarCarrito() {
---
+++ carrito.js
@@ -0,0 +1,164 @@
+const sectionCart = document.querySelector(".section-cart");
+const productosEnCarrito = JSON.parse(localStorage.getItem("carrito"));
+
+function cargarProductosCarrito() {
+  if (productosEnCarrito && productosEnCarrito.length > 0) {
+    sectionCart.innerHTML = "";
+
+    productosEnCarrito.forEach((producto) => {
+      const productoAgregado = document.createElement("article");
+      productoAgregado.classList.add("cart-wrapper");
+      productoAgregado.innerHTML = ` 
+  <div class="product-cart-img">
+    <img
+      src="${producto.imagen}"
+      alt="${producto.titulo}"
+    />
+  </div>
+
+  <div class="cart-info-wrapp">
+    <span class="product-cart-info">PRODUCTO</span>
+    <span class="product-name">${producto.titulo}</span>
+  </div>
+
+  <div class="cart-info-wrapp">
+    <span class="product-cart-info">PRECIO</span>
+    <div class="product-cart-price">
+      <span class="product-name">$${producto.precio}</span>
+    </div>
+  </div>
+  <div class="cart-info-wrapp">
+    <span class="product-cart-info">CANTIDAD</span>
+    <div class="product-cart-quantity">
+      <button class="decrement-cart">-</button>
+      <span>${producto.cantidad}</span>
+      <button class="increment-cart">+</button>
+    </div>
+  </div>
+  <div class="cart-info-wrapp">
+    <span class="product-cart-info">SUBTOTAL</span>
+    <div class="product-cart-subtotal">
+      <span class="product-name">$${producto.precio * producto.cantidad}</span>
+    </div>
+  </div>
+  <div id="${producto.id}" class="cart-trash">
+    <i class="bi bi-trash"></i>
+  </div>
+  
+`;
+      sectionCart.append(productoAgregado);
+      const decrementButton = productoAgregado.querySelector(".decrement-cart");
+      decrementButton.addEventListener("click", () =>
+        decrementCart(producto.id)
+      );
+      const incrementButton = productoAgregado.querySelector(".increment-cart");
+      incrementButton.addEventListener("click", () =>
+        incrementCart(producto.id)
+      );
+    });
+  } else {
+    sectionCart.innerHTML = "";
+    const span = document.createElement("span");
+    span.innerHTML = "Tu carrito esta vacio";
+    sectionCart.append(span);
+  }
+
+  actualizarBotonEliminar();
+}
+
+cargarProductosCarrito();
+
+function actualizarBotonEliminar() {
+  const botonEliminar = document.querySelectorAll(".cart-trash");
+  botonEliminar.forEach((boton) => {
+    boton.addEventListener("click", eliminarCarrito);
+  });
+}
+
+function eliminarCarrito(e) {
+  Toastify({
+    text: "Producto eliminado!",
+    duration: 2000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #b01212, #c94040)",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+
+  let idBoton = e.currentTarget.id;
+  const index = productosEnCarrito.findIndex(
+    (producto) => producto.id === idBoton
+  );
+  productosEnCarrito.splice(index, 1);
+  cargarProductosCarrito();
+  localStorage.setItem("carrito", JSON.stringify(productosEnCarrito));
+  compraFinalizada();
+}
+
+function decrementCart(idProducto) {
+  const index = productosEnCarrito.findIndex(
+    (producto) => producto.id === idProducto
+  );
+  if (index !== -1) {
+    if (productosEnCarrito[index].cantidad > 1) {
+      productosEnCarrito[index].cantidad--;
+    } else {
+      productosEnCarrito.splice(index, 1);
+      Toastify({
+        text: "Producto eliminado!",
+        duration: 2000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #b01212, #c94040)",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+    }
+
+    localStorage.setItem("carrito", JSON.stringify(productosEnCarrito));
+    cargarProductosCarrito();
+  }
+}
+
+function incrementCart(idProducto) {
+  const index = productosEnCarrito.findIndex(
+    (producto) => producto.id === idProducto
+  );
+  if (index !== -1) {
+    productosEnCarrito[index].cantidad++;
+    localStorage.setItem("carrito", JSON.stringify(productosEnCarrito));
+    cargarProductosCarrito();
+  }
+}
+
+const buyButton = document.querySelector(".buy");
+
+function compraFinalizada() {
+  if (productosEnCarrito && productosEnCarrito.length > 0) {
+    buyButton.addEventListener("click", () => {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Muchas gracias por su compra!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    });
+  } else {
+    buyButton.addEventListener("click", () => {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "No tienes ningun producto!",
+      });
+    });
+  }
+}
+compraFinalizada();
